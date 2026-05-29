cbb22adb7f99759728fd35f4a728c0f068b72868
yuminwu <devinwym1022@outlook.com>
2026-05-28 22:57:11 -0700

    Remove price_valley_vwap_percentile wrapper

--- factor_engine/src/factor/chn_stock/daily/price_valley_vwap_percentile.rs
+++
@@ -1,6 +0,0 @@
-use crate::factor::common::kyzq_peak_valley::PeakValleyMetric;
-
-crate::define_kyzq_peak_valley_factor!(
-    StockDailyPriceValleyVwapPercentile,
-    PeakValleyMetric::PriceValleyVwapPercentile
-);
